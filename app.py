384bcc7df7fdd48df95fb59e7ed8a80c9763fb44
Sothingguy <sothingguy@gmail.com>
2020-09-09 19:31:20 +1200

    i think so

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 if __name__ == '__main__':
-    run_app = app.create_app()
-    run_app.run(debug=True) 
+  app.run(debug=True)
 
 @app.route("/")
 def home():
