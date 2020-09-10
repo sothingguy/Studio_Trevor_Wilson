0fcb37d7c81f106e3be351fd97c88895b26340d6
sothingguy <sothingguy@gmail.com>
2020-09-10 17:59:42 +1200

    Java script responsivness added

---
+++ static/cards.js
@@ -0,0 +1,20 @@
+var data = {
+	[
+		{
+			"link": "westminster_street",
+			"images": "static/img/placeholder.jfif",
+			"text": "Westminster Street"
+		},
+		{
+			"link": "westport",
+			"images": "static/img/placeholder2.jpg",
+			"text": "Westport"
+		},
+		{
+			"link": "my_process",
+			"images": "static/img/placeholder3.jpg",
+			"text": "My Process"
+		}
+	]
+}
+export {data}
